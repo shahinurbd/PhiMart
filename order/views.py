f48c5015c60272a5fd3b98cc1c02c28825d6b120
Muhammad Shahinur <shahinurislam728@gmail.com>
2025-07-23 22:47:07 +0600

    reqirement.txt updated

--- order/views.py
+++ order/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import action, api_view
 from order.services import OrderService
 from rest_framework.response import Response
 from rest_framework import status
-from sslcommerz_lib import SSLCOMMERZ 
+from sslcommerz_lib import SSLCOMMERZ
 
 
 # Create your views here.
